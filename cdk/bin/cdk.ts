71670d0ee9b974c9a13a678b0ec6e750f72ecfb9
Nguyen Ngo <mnguyenngo@gmail.com>
2022-01-27 08:37:26 -0800

    Add CertificateStack to deploy a secure website

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import { App, Environment, Stack, StackProps } from 'aws-cdk-lib';
 import { BlogFrontendStack } from '../lib/blog-frontend-stack';
+import { CertificateStack } from '../lib/certificate-stack';
+require("dotenv").config({ path: '.env' });
 
-const app = new cdk.App();
-new BlogFrontendStack(app, 'BlogFrontendStack', {});
+const targetRegion = "us-east-1";
+
+const app = new App();
+
+class MyBlog extends Stack {
+  constructor(parent: App, name: string, props: StackProps) {
+    super(parent, name, props);
+    const certificate = new CertificateStack(this, 'CertificateStack', {
+      env: props.env as Environment,
+      domainName: 'nguyenngo.com',
+      siteSubDomain: '*',
+    })
+
+    new BlogFrontendStack(this, 'FrontendStack', {
+      env: props.env as Environment,
+      siteDomain: certificate.siteDomain,
+      viewerCertificate: certificate.viewerCertificate,
+      zone: certificate.zone,
+    });
+  }
+}
+
+new MyBlog(app, 'NguyenNgoBlog', {
+  env: {
+    region: targetRegion,
+    account: process.env.AWS_ACCOUNT,
+  },
+});
